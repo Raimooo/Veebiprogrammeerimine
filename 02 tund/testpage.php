6d1f49a6ee06bf26f5fc1064d4235c241c30ec87
Raimo Pindus <Raimooo@users.noreply.github.com>
2019-09-10 11:09:10 +0300

    esimene üleslaadimine

---
+++ 02 tund/testpage.php
@@ -0,0 +1,27 @@
+<?php
+$userName = "Raimo Pindus" ;
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $userName;
+  ?>
+  Siin ma programmeerin veebi</title>
+
+</head>
+<body>
+	<?php
+	echo "<h1>" .$userName . ", veebiprogrammeerimine </h1>";
+	
+?>
+  <h1 align="center"><p><font color="red" font size="15">Raimo Pindus, veebiprogrammeerimine</h1></font></p>
+    <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu. Aga äkki sisaldab. Kes teab? ☺</p>
+	<h1 align="center"><img src="https://i0.wp.com/www.fluxfactory.org/wp-content/uploads/2014/05/youmadeit1.jpg" ></h1>
+	<h1 align="center"><p><font color="green" font size="15"> Kui pildid ei tööta, siis proovi homme uuesti.</h1></font></p>
+    <p>Kui ei viitsi siin passida, siis mine passi <a href="http://greeny.cs.tlu.ee/~rainehal/myindex.php"> mujal </a> </p>
+</script>	
+</body>
+</html>
